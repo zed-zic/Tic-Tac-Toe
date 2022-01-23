826a5450855a4f2b0d7b76bc935e9bf14d77c9e5
zed-zic <sanchezzz1597@gmail.com>
2022-01-23 14:16:24 +0700

    create field and push symbols

--- src/com/company/Interface.java
+++ src/com/company/Interface.java
@@ -2,13 +2,29 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class Interface extends JComponent {
-//    public static void main(String[] args) {
-//        //JFrame window = getJFrame();
-//    }
+    public static final int FIELD_EMPTY = 0;
+    public static final int FIELD_X = 10;
+    public static final int FIELD_0 = 100;
+    int [][]field;
+    boolean isXTurn;
+
+
     public Interface(){
         enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+        field = new int[3][3];
+        initGame();
+    }
+
+    public void initGame(){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                field[i][j] = FIELD_EMPTY;
+            }
+        }
+        isXTurn = true;
     }
     static JFrame getJFrame(){
         JFrame window = new JFrame("tic-tac-toe");
@@ -20,10 +36,26 @@ public class Interface extends JComponent {
         return window;
     }
 
+    protected void processMouseEvent(MouseEvent mouseEvent){
+        super.processMouseEvent(mouseEvent);
+        if (mouseEvent.getButton() == mouseEvent.BUTTON1){
+            int x = mouseEvent.getX();
+            int y = mouseEvent.getY();
+            int i = (int) ((float)x / getWidth() * 3);
+            int j = (int) ((float)y / getHeight() * 3);
+            if (field[i][j] == FIELD_EMPTY){
+                field[i][j] = isXTurn?FIELD_X:FIELD_0;
+                isXTurn = !isXTurn;
+                repaint();
+            }
+        }
+    }
+
     protected void paintComponent(Graphics graphics){
         super.paintComponent(graphics);
         drawField(graphics);
     }
+
     void drawField(Graphics graphics){
         int width = getWidth();
         int height = getHeight();
@@ -34,4 +66,6 @@ public class Interface extends JComponent {
             graphics.drawLine(width / 3 * i,0,width / 3 * i, height);
         }
     }
+
+
 }
