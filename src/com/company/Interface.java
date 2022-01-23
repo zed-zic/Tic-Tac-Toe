771490322adef6335c6338d104424dd7059d08fa
zed-zic <sanchezzz1597@gmail.com>
2022-01-23 13:46:14 +0700

    printField

--- src/com/company/Interface.java
+++ src/com/company/Interface.java
@@ -3,17 +3,29 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 
-public class Interface {
-    public static void main(String[] args) {
-        JFrame window = getJFrame();
-    }
+public class Interface extends JComponent {
+//    public static void main(String[] args) {
+//        //JFrame window = getJFrame();
+//    }
     static JFrame getJFrame(){
         JFrame window = new JFrame("tic-tac-toe");
         window.setVisible(true);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
-        window.setBounds(dimension.width/2-250, dimension.height/2-150, 500, 300);
+        window.setBounds(dimension.width/2-250, dimension.height/2-150, 450, 450);
         return window;
+    }
+
+    protected void paintComponent(Graphics graphics){
+        int width = getWidth();
+        int height = getHeight();
+        super.paintComponent(graphics);
+        graphics.setColor(Color.red);
+//        graphics.drawOval(10,10,100,100);
+        for (int i = 1; i < 3; i++) {
+            graphics.drawLine(0,height / 3 * i,width,height / 3 * i);
+            graphics.drawLine(width / 3 * i,0,width / 3 * i, height);
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        JFrame window = Interface.getJFrame();
+        Interface game = new Interface();
+        window.add(game);
     }
 }
